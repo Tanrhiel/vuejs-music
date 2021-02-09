01a7fd173d026dd4eb571bf8117a3a131767fe88
kennyljd <k.louis-joseph-dogue@ipeos.com>
2021-02-09 11:46:57 -0400

    première interface

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,81 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="flex">
+    <div class="flex-auto">
+      <div class="p-6 max-w-xl mx-auto bg-white rounded-xl shadow-md flex items-center space-x-4">
+        <div class="flex-shrink-0">
+          <img class="h-12 w-12" src={{current.image}} alt="Album">
+        </div>
+        <div>
+          <h2 class="text-blue-600 md:text-green-600 ">{{ current.title }} <span>{{ current.artist }}</span></h2>
+          <div class="flex">
+            <span class="flex-1"> <i class="fas fa-arrow-circle-left md:text-green-600"></i> </span>
+            <span class="flex-1"> <i class="fas fa-play-circle md:text-green-600" v-if="!isPlaying" @click="play"></i> </span>
+            <span class="flex-1"> <i class="fas fa-pause-circle md:text-green-600" @click="pause" ></i> </span>
+            <span class="flex-1"> <i class="fas fa-arrow-circle-right md:text-green-600"></i> </span>
+          </div>
+        </div>
+      </div>          
+
+    </div>
+    <div class="flex-1">
+      <div class="p-6 max-w-xl mx-auto bg-white rounded-xl shadow-md flex items-center space-x-4">
+        <div>
+          <h2 class="text-blue-600 md:text-green-600 ">PlayList</h2>
+          <p v-for="song in songs" :key="song.src" @click="play(song)" :class="(song.src == current.src) ? 'song playing':'song'">
+            {{ song.title }} - <span>{{ song.artist }}</span>
+          </p>            
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+// import Nav from './components/Nav.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data(){
+    return{
+      current:{},
+      index:0,
+      isPlaying: false,
+      songs:[
+        {
+        title: 'Trouble Scene',
+        artist: 'MWD',
+        src: require('./assets/songs/musics/music_397.mp3'),
+        image:require('./assets/songs/images/vinyl.jpeg'),
+      },
+      {
+        title: 'A little Christmas music',
+        artist: 'Lena Orsa',
+        src: require('./assets/songs/musics/music_397.mp3'),
+        image:require('./assets/songs/images/lenaOrsa.jpeg')
+      },
+      ],
+      player:new Audio()
+    }
+  },
+  methods:{
+    play(song){
+      if(song.src !="undefined"){
+        this.current = song;
+        this.player.src = this.current.src;
+      }
+      this.player.play();
+      this.isPlaying = true;
+    },
+    pause(){
+      this.player.pause();
+      this.isPlaying = false;
+    }
+  },
+  created(){
+    this.current = this.songs[this.index];
+    this.player.src = this.current.src;
+  
   }
 }
 </script>
---
+++ src/components/Nav.vue
@@ -0,0 +1,11 @@
+<template>
+    <p>Menu</p>
+    <div class="lg:absolute lg:inset-y-0 lg:right-1/10 lg:w-1/2">
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Nav'
+}
+</script>
